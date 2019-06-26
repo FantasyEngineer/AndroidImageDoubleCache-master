a00c40362853394f91743f00bba82798bf4248af
FantasyEngineer <347622814@qq.com>
2019-06-26 13:58:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/paul/cache/util/DoubleCache.java
@@ -0,0 +1,46 @@
+package com.example.paul.cache.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+import com.example.paul.cache.util.libcore.DiskCache;
+import com.example.paul.cache.util.libcore.ImageCache;
+import com.example.paul.cache.util.libcore.MemoryCache;
+
+/**
+ * 双缓存操作类
+ * Created by paul on 15/12/28.
+ */
+public class DoubleCache implements ImageCache {
+    private static final String TAG = DoubleCache.class.getSimpleName();
+    private MemoryCache mMemoryCache = null;
+    private DiskCache mDiskCache = null;
+
+    public DoubleCache(Context context){
+        mMemoryCache = new MemoryCache();
+        mDiskCache = new DiskCache(context);
+    }
+    @Override
+    public Bitmap get(String url) {
+        String key = url2Key(url);
+        Bitmap bitmap = mMemoryCache.get(key);
+        if(bitmap==null){
+            bitmap = mDiskCache.get(key);
+        }else {
+        }
+        return bitmap;
+     }
+
+    @Override
+    public void put(String url, Bitmap bitmap) {
+        String key = url2Key(url);
+        mMemoryCache.put(key,bitmap);
+        mDiskCache.put(key,bitmap);
+    }
+
+    //url转key
+    private String url2Key(String url){
+        String key = MD5.hashKeyForDisk(url)+".jpg";
+        return key;
+    }
+}
